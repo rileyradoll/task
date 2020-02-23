c265ac143c01640f3e6187f439ef5ebfb97b4aff
Riley Radoll <rileyradoll@gmail.com>
2020-02-23 16:19:57 -0500

    Create image and other resources
    Co-authored-by: Bray789 <austinabray@gmail.com>

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,10 @@
+class ImagesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'images/index'
+  get 'images/new'
+  get 'images/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htmlr
   root 'welcome#index'
   resources :gallery, only: [:index]
