84bc0379df142f94e68184ac51bc0f2f8c374cd9
Riley Radoll <rileyradoll@gmail.com>
2020-02-23 16:05:20 -0500

    Update landing url to use welcome page
    Co-authored-by: Bray789 <austinabray@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :welcome, only: [:index]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htmlr
+  root 'welcome#index'
   resources :gallery, only: [:index]
 end
